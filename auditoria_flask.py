15e468a2d1c3c09614b1091ca6c50c96acf1d188
Geovane Leite <122981364+Geovane-Leite@users.noreply.github.com>
2024-02-26 14:34:53 -0300

    Add files via upload

--- auditoria_flask.py
+++ auditoria_flask.py
@@ -11,12 +11,13 @@ def ler_pdf(nome_arquivo):
     for numero_pagina in range(documento.page_count):
         pagina = documento.load_page(numero_pagina)
         texto_pagina = pagina.get_text()
-        padrao = r'\.\s*\n' # identifica final de cada paragrafo.
-        texto_pagina = re.sub(padrao, lambda m: f". (Pag. {numero_pagina+1}).\n", texto_pagina) # adiciona a pagina ao paragrafo
+        padrao = r'\.\s*\n'
+        texto_pagina = re.sub(padrao, lambda m: f". (Pag. {numero_pagina+1}).\n", texto_pagina)
         texto += texto_pagina
     return texto
 
 def encontrar_paragrafos(texto, consulta):
+    texto = texto.replace('- \n','').replace('-\n','')
     paragrafos = texto.split('.\n')
     consulta_palavras = consulta.lower().split()
     
@@ -48,43 +49,47 @@ def index():
 
 def pesquisar(consulta):
     cont = None
-    diretorio = r'\\projeto\docs\\manuais' # diretorio pdf
+    diretorio = r'\\10.8.0.12\contas medicas\Ferramentas Python\manuais'
     resultado_text = ""
     consulta = consulta.replace(',', ' ')
     for arquivo in os.listdir(diretorio):
         nome_arquivo = os.path.join(diretorio, arquivo)
-        nome_arquivo_txt = os.path.join(diretorio, arquivo).replace('manuais', 'temp_manuais').replace('.pdf', '.txt') # diretorio txt
-        if not os.path.isfile(nome_arquivo_txt): # Abrir PDF caso TXT não exista
+        nome_arquivo_txt = os.path.join(diretorio, arquivo).replace('manuais', 'temp_manuais').replace('.pdf', '.txt')
+        if not os.path.isfile(nome_arquivo_txt):
             texto_pdf = ler_pdf(nome_arquivo)
             with open(nome_arquivo_txt, 'w', encoding='utf-8') as txt_file:
                 txt_file.write(texto_pdf)
             resultados = encontrar_paragrafos(texto_pdf, consulta)
         else:
-            with open(nome_arquivo_txt, 'r', encoding='utf-8') as txt_file:  # Abrir TXT
+            with open(nome_arquivo_txt, 'r', encoding='utf-8') as txt_file:
                 texto_txt = txt_file.read()
             resultados = encontrar_paragrafos(texto_txt, consulta)
 
         # Verifica se há resultados
         if resultados and resultados != ["Nenhum parágrafo encontrado."]:
             cont = 'encontrou'
-            resultado_text += f'<h3 style="background-color: lightgray;">Nome do Arquivo: {arquivo}</h3>'
+            resultado_text += f'<h3 style="background-color: green; color: white; font-family: Arial, sans-serif;">{arquivo}</h3>'
             resultados_ordenados = sorted(resultados, key=lambda x: x[1], reverse=True)
             cont_2 = 0
             for resultado in resultados_ordenados:
                 cont_2 +=1
                 try:
                     paragrafo, cont_palavras_encontradas, palavras_encontradas = resultado
-                    if cont_palavras_encontradas >= 1 and cont_2==1:
-                        resultado_text += f"<h4>Parágrafos encontrados: {len(resultados)}</h4>"
-                    resultado_text += f"<p>{paragrafo}</p><br/><p>Total Palavras Encontradas: {cont_palavras_encontradas} : {' ; '.join(palavras_encontradas)}</p><br/>"
-                    resultado_text += '<hr style="border: 0.5mm solid gray; width: 100%; height: 100%;">'
 
+                    if cont_palavras_encontradas >= 1 and cont_2==1:
+                        resultado_text += f"<h4 style='font-family: Arial, sans-serif;'><span style='background-color: rgba(144, 238, 144, 0.5); color: green;'>{'Parágrafo encontrado' if len(resultados) == 1 else 'Parágrafos encontrados'}: {len(resultados)}</span></h4>"
+                    resultado_text += '<div style="background-color: lightgray; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1); padding: 10px;">'
+                    resultado_text += f"<p style='font-family: Arial, sans-serif;'>{paragrafo}</p><p style='font-family: Arial, sans-serif;'>Total Palavras Encontradas: {cont_palavras_encontradas} : {' ; '.join(palavras_encontradas)}</p>"
+                    resultado_text += '</div>'
+                    resultado_text += '<hr style="border: 0.5mm gray; width: 100%; height: 100%;">'
+
+                    
                 except:
                     #resultado_text += f"Pesquisa {consulta} não encontrada.\n"
                     continue
     else:
         if not cont:
-            resultado_text += f"<h3>Pesquisa {consulta} não encontrada!<h3>" 
+            resultado_text += f'<h3 style="background-color: red; color: white; font-family: Arial, sans-serif;">Pesquisa {consulta} não encontrada!</h3>'
     return resultado_text
 
 
